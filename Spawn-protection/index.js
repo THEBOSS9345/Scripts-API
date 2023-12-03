22a359c12c04374b33f5f05fdd6640eb9fd54365
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2023-12-02 22:15:39 -0800

    v2.0

--- Spawn-protection/index.js
+++ Spawn-protection/index.js
@@ -1,26 +1,22 @@
-import { BlockVolumeUtils, BoundingBoxUtils, Vector, world, system } from '@minecraft/server';
+import { world, Player } from '@minecraft/server';
+const database = [];
 
-class SpawnPro {
-    constructor() {
-        this.box = (players, res) => players.some(player => BoundingBoxUtils.isInside(BoundingBoxUtils.createValid(res.data.from, res.data.to), player.location));
-        this.spawnLocations = [];
-        this.playersInside = new Set();
-        system.runInterval(() => this.updatePlayerLocations());
-        world.beforeEvents.itemUseOn.subscribe((data) => this.playersInside.has(data.source.name) ? (data.cancel = true) : null);
-        world.beforeEvents.playerBreakBlock.subscribe((data) => this.playersInside.has(data.player.name) ? (data.cancel = true) : null);
-    }
-    setSpawn(locations) {
-        this.spawnLocations = locations.map((location) => ({ from: new Vector(location.from.x, location.from.y, location.from.z), to: new Vector(location.to.x, location.to.y, location.to.z) }));
-    }
-    updatePlayerLocations() {
-        for (const player of world.getPlayers()) {
-            const isInsideAnyBox = this.spawnLocations.some((location) => this.box([player], { data: { from: location.from, to: location.to } }));
-            if (isInsideAnyBox) this.playersInside.add(player.name); else this.playersInside.delete(player.name);
-        }
-    }
-}
-
-const spawnProInstance = new SpawnPro();
-spawnProInstance.setSpawn([
-    { from: { x: -116, y: -41, z: 186 }, to: { x: 123, y: 207, z: -73 } },
-]); 
+  /**
+   * @param {{ cords1: {x: number, y: number, z: number}, cords2: {x: number, y: number, z: number}}} location
+   * @param {((player: Player) => boolean) } permissions
+   */ function SpawnPro(location, permissions) {
+    database.push({ location: location, permissions: permissions });
+    const box = (player, location) =>  player.location.x >= Math.min(location.cords1.x, location.cords2.x) && player.location.x <= Math.max(location.cords1.x, location.cords2.x) && player.location.y >= Math.min(location.cords1.y, location.cords2.y) && player.location.y <= Math.max(location.cords1.y, location.cords2.y) && player.location.z >= Math.min(location.cords1.z, location.cords2.z) && player.location.z <= Math.max(location.cords1.z, location.cords2.z);
+    world.beforeEvents.playerBreakBlock.subscribe((data) => {
+      const isInsideAnyLocation = database.filter((value) => box(data.block, value.location))[0]
+      if (!isInsideAnyLocation) return
+      isInsideAnyLocation.permissions ? isInsideAnyLocation.permissions(data.player) ? null : data.cancel = true : data.cancel = true
+    })
+    world.beforeEvents.playerInteractWithBlock.subscribe((data) => {
+        const isInsideAnyLocation = database.filter((value) => box(data.block, value.location))[0]
+        if (!isInsideAnyLocation) return
+        isInsideAnyLocation.permissions ? isInsideAnyLocation.permissions(data.player) ? null : data.cancel = true : data.cancel = true
+    })
+  }
+// Example usage
+SpawnPro({ cords1: { x: 80, y: -61, z: 80 }, cords2: { x: 900, y: 900, z: 900 }}, ((player) => player.hasTag('diamond') ));
