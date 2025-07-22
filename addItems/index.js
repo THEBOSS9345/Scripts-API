dcb658f04371074a70d7157b6927843c5fe15070
THE_BOSS9345 <125090249+THEBOSS9345@users.noreply.github.com>
2025-07-22 17:46:38 -0400

    Update index.js

--- addItems/index.js
+++ addItems/index.js
@@ -7,18 +7,20 @@ import { Player, system, ItemStack, Enchantment } from "@minecraft/server";
  * @param {Array<[Item, number, Array<Enchantment>]>} items - An array of items to add, where each item is represented by [item, count, enchants].
  * @param {Array<Enchantment>=} enchants - Optional array of enchantments to apply to all items.
  */
-
 function addItems(player, items) {
     system.run(() => {
         const inv = player.getComponent('inventory').container;
+    
         for (let [item, count, enchants] of items) {
+        
             const itemStack = new ItemStack(item, count)
+          
             if (enchants && enchants.length > 0) {
                 const enchantComp = itemStack.getComponent("enchantable")
-                for (const enc of enchants) console.log(JSON.stringify(enc))
                 for (const enchant of enchants) enchantComp.addEnchantment(enchant);
             }
+
             inv.addItem(itemStack);
         }
-    })
+    });
 }
